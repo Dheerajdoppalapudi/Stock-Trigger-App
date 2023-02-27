a73247c9fe915b85184a70ee159a1094af91aeb2
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2023-02-27 23:30:55 +0530

    users added

--- trigger/users/urls.py
+++ trigger/users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('users/register/', views.registersUser, name='register'),
     path('users/login/', views.loginUser, name='login'),
+    path('users/logout/', views.logoutRequest, name='logout'),
 ]
--- trigger/users/views.py
+++ trigger/users/views.py
@@ -23,4 +23,20 @@ def registersUser(request):
 
 
 def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            messages.info(request, "Username or Password is Incorrect ")
+        
     return render(request, 'users/login.html')
+
+def logoutRequest(request):
+    logout(request)
+    return redirect("login")
